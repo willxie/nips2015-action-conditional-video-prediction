7d83bdedbaa30e8cfe9fa706ed90f6e35fe71262
William Xie <wxie@cs.utexas.edu>
2016-03-28 04:06:45 -0500

    Added 'act.log' converting script

---
+++ scripts/convert_action_keys.py
@@ -0,0 +1,36 @@
+import os
+
+def is_int(s):    
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def main(): 
+    input_file = "pong_rand_actions.txt"
+    num_actions = 0
+    container = {}
+    
+    filename, file_extension = os.path.splitext(input_file)
+    with open(input_file, "rb") as r, open("act.log", "wb") as w:
+        for line in r:
+            line_s = line.rstrip("\n")
+            if (is_int(line_s)):
+                action_num = int(line_s)
+                if (action_num not in container):        
+                    container[action_num] = num_actions
+                    num_actions += 1
+                w.write(str(container[action_num]))
+                w.write("\n")
+    
+    
+    # Write the keys
+    with open(filename + "_key" + file_extension, "wb") as f:
+        f.write("orig" + "\t=>\t" + "new" + "\n")
+        for key, value in container.iteritems():
+            f.write(str(key) + "\t=>\t" + str(value) + "\n")
+    
+if __name__ == "__main__":
+    main()
